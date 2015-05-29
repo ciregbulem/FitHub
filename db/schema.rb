f68b0c188727f3a61ca8e8604a6ba85620f16572
ciregbulem <chudi.iregbulem@trincoll.edu>
2015-05-28 21:07:50 -0700

    Attempting to migrate on Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,55 @@ ActiveRecord::Schema.define(version: 20150423040700) do
   enable_extension "plpgsql"
   enable_extension "hstore"
 
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "fname"
+    t.string   "lname"
+    t.text     "about"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "image"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string   "temp_email"
+    t.boolean  "verified"
+    t.hstore   "info"
+    t.string   "fb_link"
+    t.string   "tw_link"
+    t.string   "ig_link"
+    t.string   "oauth_token"
+    t.string   "oauth_secret"
+    t.string   "oauth_expires_at"
+    t.integer  "admin_id"
+    t.string   "fitbit_id"
+    t.string   "fitbit_image"
+    t.string   "daily_cals_goal"
+    t.string   "daily_steps_goal"
+    t.string   "daily_dist_goal"
+    t.string   "todays_cals"
+    t.string   "todays_steps"
+    t.string   "todays_dist"
+    t.string   "todays_sleep"
+    t.string   "insurance_provider"
+  end
+
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -79,54 +128,6 @@ ActiveRecord::Schema.define(version: 20150423040700) do
 
   add_index "rewards", ["admin_id"], name: "index_rewards_on_admin_id", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "fname"
-    t.string   "lname"
-    t.text     "about"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "image"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-    t.string   "temp_email"
-    t.boolean  "verified"
-    t.hstore   "info"
-    t.string   "fb_link"
-    t.string   "tw_link"
-    t.string   "ig_link"
-    t.string   "oauth_token"
-    t.string   "oauth_secret"
-    t.string   "oauth_expires_at"
-    t.integer  "admin_id"
-    t.string   "fitbit_id"
-    t.string   "fitbit_image"
-    t.string   "daily_cals_goal"
-    t.string   "daily_steps_goal"
-    t.string   "daily_dist_goal"
-    t.string   "todays_cals"
-    t.string   "todays_steps"
-    t.string   "todays_dist"
-    t.string   "todays_sleep"
-    t.string   "insurance_provider"
-  end
 
   add_index "users", ["admin_id"], name: "index_users_on_admin_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
